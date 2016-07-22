b83ff64c1f9c47bde2833cb9116fbe2fd083215e
lstearns86 <lstearns86@gmail.com>
2016-07-22 15:43:40 -0400

    Refactored Gesture Recognition Demo

--- HandSightOnBodyInteractionNoGPU/GestureRecognitionTest.Designer.cs
+++ HandSightOnBodyInteractionNoGPU/GestureRecognitionTest.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HandSightOnBodyInteractionNoGPU
 {
-    partial class Form1
+    partial class GestureRecognitionTest
     {
         /// <summary>
         /// Required designer variable.
@@ -30,10 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.Text = "GestureRecognitionTest";
         }
 
         #endregion
     }
 }
-
